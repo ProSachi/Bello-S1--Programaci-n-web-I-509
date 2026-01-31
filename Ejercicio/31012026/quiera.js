932bd8a20f0a0432b85598168965e45e048941c3
ProSachi <syosa@cesde.net>
2026-01-31 13:02:47 -0500

    Semana 1

---
+++ Ejercicio/31012026/quiera.js
@@ -0,0 +1,21 @@
+// Variables
+
+/* let; 
+const; */
+
+// Tipos de datos
+// number String Booleanos
+
+let nombre = "Santiago";
+let edad = 10;
+let telefono = "3123123134";
+
+console.log(nombre);
+console.log(edad);
+console.log(telefono);
+
+let nombre2 = "Maria";
+let apellido = "Suarez"
+
+console.log("El nombre de la persona es:" + nombre2 + "y el apellido es: "+ apellido);
+console.log(`El nombre de la persona es ${nombre2} y el apellido es ${apellido}`);
